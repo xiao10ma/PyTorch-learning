b4c2ef57ac78a0a901fd49d2d5f147a3d32a7401
xiaoma <mazp@mail2.sysu.edu.cn>
2024-05-22 15:35:37 +0800

    add advanced_rnn

--- 12_basic_rnn.py
+++ 12_basic_rnn.py
@@ -48,11 +48,13 @@ RNN
 # print('Hidden size: ', hidden.shape)
 # print('Hidden: ', hidden)
 
+num_class = 4
 batch_size = 1
-seq_len = 3
+seq_len = 5
 input_size = 4
-hidden_size = 4
-num_layers = 1
+hidden_size = 8
+embedding_size = 10
+num_layers = 2
 
 idx2char = ['e', 'h', 'l', 'o']
 x_data = [1, 0, 2, 2, 3]
@@ -67,7 +69,7 @@ one_hot_lookup = [
 
 x_one_hot = [one_hot_lookup[x] for x in x_data]
 
-inputs = torch.Tensor(x_one_hot).view(-1, batch_size, input_size)       
+inputs = torch.LongTensor(x_one_hot).view(batch_size, -1, input_size)       
 labels = torch.LongTensor(y_data)
 
 '''
@@ -83,30 +85,72 @@ hidden  [num_layers, batch_size, hidden_size]   最后一个RNN的输出
 
 '''
 
-class Model(torch.nn.Module):
-    def __init__(self, input_size, hidden_size, batch_size, num_layers=1):
-        super(Model, self).__init__()
-        self.num_layers = num_layers
-        self.batch_size = batch_size
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.rnn = torch.nn.RNN(input_size=self.input_size,
-                                        hidden_size=self.hidden_size,
-                                        num_layers=num_layers)
+# class Model(torch.nn.Module):
+#     def __init__(self, input_size, hidden_size, batch_size, num_layers=1):
+#         super(Model, self).__init__()
+#         self.num_layers = num_layers
+#         self.batch_size = batch_size
+#         self.input_size = input_size
+#         self.hidden_size = hidden_size
+#         self.rnn = torch.nn.RNN(input_size=self.input_size,
+#                                         hidden_size=self.hidden_size,
+#                                         num_layers=num_layers)
         
-    def forward(self, input):
-        hidden = torch.zeros(self.num_layers, self.batch_size, self.hidden_size)
-        out, _ = self.rnn(input, hidden)
-        return out.view(-1, self.hidden_size)
+#     def forward(self, input):
+#         hidden = torch.zeros(self.num_layers, self.batch_size, self.hidden_size)
+#         out, _ = self.rnn(input, hidden)
+#         return out.view(-1, self.hidden_size)
     
-net = Model(input_size, hidden_size, batch_size)
+# net = Model(input_size, hidden_size, batch_size)
+
+# criterion = torch.nn.CrossEntropyLoss()
+# optimizer = torch.optim.Adam(net.parameters(), lr=0.05)
+
+# for epoch in range(15):
+#     optimizer.zero_grad()
+#     outputs = net(inputs)
+#     loss = criterion(outputs, labels)
+#     loss.backward()
+#     optimizer.step()
+
+#     _, idx = outputs.max(dim=1)
+
+#     idx = idx.data.numpy()
+#     print('Predicted: ', ''.join([idx2char[x] for x in idx]), end='')
+#     print(', Epoch [%d/15] loss = %.4f' % (epoch + 1, loss.item()))
+
+
+'''
+Add embedding
+'''
+
+
+
+class Model(torch.nn.Module):
+    def __init__(self, ):
+        super(Model, self).__init__()
+        self.emb = torch.nn.Embedding(input_size, embedding_size)
+        self.rnn = torch.nn.RNN(input_size=embedding_size,
+                                        hidden_size=hidden_size,
+                                        num_layers=num_layers,
+                                        batch_first=True)
+        self.fc = torch.nn.Linear(hidden_size, num_class)
+
+    def forward(self, x):
+        hidden = torch.zeros(num_layers, x.size(0), hidden_size)
+        x = self.emb(x)
+        x, _ = self.rnn(x, hidden)
+        x = self.fc(x)
+        return x.view(-1, num_class)
+    
+net = Model()
 
 criterion = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(net.parameters(), lr=0.05)
 
 for epoch in range(15):
     optimizer.zero_grad()
-    outputs = net(inputs)
+    outputs = net(torch.LongTensor(x_data).view(batch_size, seq_len))
     loss = criterion(outputs, labels)
     loss.backward()
     optimizer.step()
